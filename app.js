9df516dda6ca66bb821fe73a4d287eb703428c34
Panurat <paunavt5@gmail.com>
2024-07-30 20:01:48 +0700

    add path html

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // app.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define the route for the homepage
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
